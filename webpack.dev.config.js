bb824826504340072512212a33e0dd5075b0999d
DavidMBP <steadypathapp@gmail.com>
2017-06-30 17:40:53 -0500

    Created the webpack dev config

---
+++ webpack.dev.config.js
@@ -0,0 +1,56 @@
+var webpack = require('webpack')
+var path = require('path')
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+
+var config = {
+  entry: './frontend/main.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+			'frontend',
+      'frontend/components'
+    ],
+    extensions: ['.js', '.jsx', '.scss', '.sass']
+  },
+  module: {
+    rules: [{
+      test: /\.(jsx|js)?$/,
+      exclude: /(node_modules)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2016', 'react'],
+          plugins: ["transform-decorators-legacy"]
+        }
+      }]
+    },{
+      test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      loader: 'file-loader?name=fonts/[name].[ext]'
+    },{
+      test: /\.(scss|sass|css)$/,
+      use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })
+    }]
+  },
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    publicPath: '/',
+    hot: true,
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+  ],
+}
+
+module.exports = config
