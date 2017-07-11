f24be035cb4e6fe8e6a0261d59d9b6069a5bd1db
DavidMBP <steadypathapp@gmail.com>
2017-07-11 15:01:21 -0500

    Started the terminal action

---
+++ frontend/actions/terminal.js
@@ -0,0 +1,9 @@
+import * as types from 'actions/types'
+
+export const commandEntered = (command) => {
+
+	return {
+		type: types.COMMAND_ENTERED,
+		payload: {}
+	}
+}
---
+++ frontend/actions/types.js
@@ -0,0 +1,2 @@
+
+export const COMMAND_ENTERED = 'COMMAND_ENTERED'
