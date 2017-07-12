5dc34bd6ef791f77229aa603e43ddba30c1eb907
DavidMBP <steadypathapp@gmail.com>
2017-07-11 22:09:34 -0500

    Handled the COMMAND_ENTERED reducer

--- frontend/reducers/terminal.js
+++ frontend/reducers/terminal.js
@@ -1,10 +1,18 @@
 import { fromJS } from 'immutable'
-
+import * as types from 'actions/types'
 
 const INITIAL_TERMINAL = fromJS([])
 
 
 export const terminalReducer = (state = INITIAL_TERMINAL, action) => {
-	
+
+	if(action.type === `${types.COMMAND_ENTERED}_ERROR`){
+		return state.push(fromJS(action.payload.data))
+	}
+
+	if(action.type === `${types.COMMAND_ENTERED}_SUCCESS`){
+		return state.push(fromJS(action.payload.data))
+	}
+
 	return state
 }
