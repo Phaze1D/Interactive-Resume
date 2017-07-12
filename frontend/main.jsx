8a675999cd2603e64daaa44b92dff8a4c9248107
DavidMBP <steadypathapp@gmail.com>
2017-07-11 22:10:05 -0500

    Added Redux Dev Tools config

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -5,11 +5,30 @@ import { applyMiddleware, createStore } from 'redux'
 import promise from 'redux-promise-middleware'
 import { getReducers } from 'reducers'
 import Layout from 'Layout'
+import Immutable from 'immutable'
 
 import 'sass/index.sass'
 
 let middleware = applyMiddleware( promise({promiseTypeSuffixes: ['LOADING', 'SUCCESS', 'ERROR']}))
-const store = createStore(getReducers(), middleware)
+let store = null
+
+
+if(process.env.NODE_ENV === 'production'){
+	//
+}else{
+	const composeEnhancers = typeof window === 'object' && typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined' ?
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+			serialize: {
+				immutable: Immutable
+			}
+		}) : null
+
+	store = createStore(
+		getReducers(),
+		composeEnhancers(middleware)
+	)
+}
+
 
 ReactDOM.render(
 	<Provider store={store}>
