85af1c8c6355d6857182feca32229ae2c69b03a9
DavidMBP <steadypathapp@gmail.com>
2017-07-12 22:31:18 -0500

    Created the new TerminalItem Component

---
+++ frontend/components/dumb/Terminal/items/Item.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+/**
+* Terminal Item Component
+* Parent Component for all terminal items
+*/
+const TerminalItem = ({children, withInput, command}) => (
+	<div className={`input-item ${withInput ? 'with-input' : ''}`}>
+		<p>
+			<span className='orange'> david </span> at
+			<span className='yellow'> Joker </span> in
+			<span className='green'> ~/Projects/Udacity/InteractiveResume </span> on
+			<span className='purple'> master </span>
+		</p>
+
+		{withInput ?
+			<div className='input-area'>
+				{children}
+			</div>
+			:
+			<div className='input-area'>
+				<span className='close left'>[</span>
+					$<p>{command}</p>
+				<span className='close right'>]</span>
+			</div>
+		}
+
+		{!withInput && children}
+	</div>
+)
+
+export default TerminalItem
