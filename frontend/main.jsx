151ac886faab47004e7d1f6b03ce83632f79fe0f
DavidMBP <steadypathapp@gmail.com>
2017-07-12 22:26:25 -0500

    Added Extract CSS hot reloader hack

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -36,3 +36,22 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('app')
 )
+
+
+if (module.hot) {
+	const hotEmitter = require('webpack/hot/emitter')
+	const DEAD_CSS_TIMEOUT = 2000
+
+	hotEmitter.on('webpackHotUpdate', function(currentHash) {
+		document.querySelectorAll('link[href][rel=stylesheet]').forEach((link) => {
+			const nextStyleHref = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`)
+			const newLink = link.cloneNode()
+			newLink.href = nextStyleHref
+
+			link.parentNode.appendChild(newLink)
+			setTimeout(() => {
+				link.parentNode.removeChild(link)
+			}, DEAD_CSS_TIMEOUT)
+		})
+	})
+}
