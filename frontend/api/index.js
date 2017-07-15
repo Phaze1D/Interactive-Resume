8e41b6f66bf90b7a1cdaef351e37c325820c3146
DavidMBP <steadypathapp@gmail.com>
2017-07-14 22:24:22 -0500

    Removed Github API

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,10 +1,5 @@
-import github from './github'
 import local from  './local'
 
 export default function getCommandInfo(command) {
-	if(command.includes('projects')){
-		return github.get('projects')
-	}else{
-		return local.get(command)
-	}
+	return local.get(command)
 }
