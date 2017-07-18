bff27cc971ab9fff0a51635afe658e3b56f06153
DavidMBP <steadypathapp@gmail.com>
2017-07-17 22:00:38 -0500

    Added ADD_TAB and REMOVE_TAB actions

--- frontend/actions/terminal.js
+++ frontend/actions/terminal.js
@@ -1,10 +1,29 @@
 import * as types from 'actions/types'
 import getCommandInfo from 'api'
 
-export const commandEntered = (command) => {
-
+export const commandEntered = (command, tabID) => {
 	return {
 		type: types.COMMAND_ENTERED,
-		payload: getCommandInfo(command.toLowerCase())
+		payload: getCommandInfo(command.toLowerCase()),
+		meta: tabID
+	}
+}
+
+
+export const addTab = (location) => {
+	return {
+		type: types.ADD_TAB,
+		payload: {
+			location: location ? location : '~/Projects/InteractiveResume',
+			tabLog: []
+		}
+	}
+}
+
+
+export const removeTab = (id) => {
+	return {
+		type: types.REMOVE_TAB,
+		payload: {id: id}
 	}
 }
--- frontend/actions/types.js
+++ frontend/actions/types.js
@@ -1,2 +1,4 @@
 
 export const COMMAND_ENTERED = 'COMMAND_ENTERED'
+export const ADD_TAB = 'ADD_TAB'
+export const REMOVE_TAB = 'REMOVE_TAB'
