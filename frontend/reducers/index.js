ce01835f718f78b41df09261e1181c36e06a3dcc
DavidMBP <steadypathapp@gmail.com>
2017-07-17 22:15:11 -0500

    Added addTabReducer and removeTabReducer

--- frontend/reducers/index.js
+++ frontend/reducers/index.js
@@ -1,21 +1,29 @@
-import { combineReducers } from 'redux-immutable'
 import Immutable from 'immutable'
-// import reduceReducers from 'reduce-reducers'
-
-
-import { terminalReducer } from 'reducers/terminal'
+import reduceReducers from 'reduce-reducers'
+import { combineReducers } from 'redux-immutable'
 import { appleMenuReducer } from 'reducers/apple_menu'
+import {
+	commandReducer,
+	addTabReducer,
+	removeTabReducer
+} from 'reducers/terminal'
 
 
+
+const terminalReducer = reduceReducers(
+	commandReducer,
+	addTabReducer,
+	removeTabReducer
+)
 
 const StateRecord = Immutable.Record({
-	terminalLog: undefined,
+	terminalTabs: undefined,
 	appleMenuItems: undefined
 })
 
 export const getReducers = (preloadState=StateRecord) => {
 	return combineReducers({
-		terminalLog: terminalReducer,
+		terminalTabs: terminalReducer,
 		appleMenuItems: appleMenuReducer
 	}, preloadState)
 }
--- frontend/reducers/terminal.js
+++ frontend/reducers/terminal.js
@@ -1,21 +1,45 @@
 import { fromJS } from 'immutable'
 import * as types from 'actions/types'
+import uuid from 'uuid/v4'
 
-const INITIAL_TERMINAL = fromJS([])
+
+const INITIAL_TERMINAL = fromJS({
+	'0': {
+		location: '~/Projects/InteractiveResume',
+		tabLog: []
+	},
+	'122': {
+		location: '~/Projects/InteractiveResume',
+		tabLog: []
+	}
+})
 
 
-export const terminalReducer = (state = INITIAL_TERMINAL, action) => {
 
-	if(action.type === `${types.COMMAND_ENTERED}_ERROR`){
-		return state.push(fromJS(action.payload.data))
+export const commandReducer = (state = INITIAL_TERMINAL, action) => {
+	if(action.type.includes('SUCCESS') || action.type.includes('ERROR')){
+		let id = action.meta
+		if(action.payload.data.command === 'intro'){
+			return state.setIn([id, 'tabLog'], fromJS([action.payload.data]))
+		}
+		return state.updateIn([id, 'tabLog'], l => l.push(fromJS(action.payload.data)))
 	}
 
-	if(action.type === `${types.COMMAND_ENTERED}_SUCCESS`){
+	return state
+}
 
-		if(action.payload.data.command === 'intro'){
-			return INITIAL_TERMINAL.push(fromJS(action.payload.data))
-		}
-		return state.push(fromJS(action.payload.data))
+export const addTabReducer = (state = INITIAL_TERMINAL, action) => {
+	if(action.type === types.ADD_TAB){
+		return state.set(uuid(),fromJS(action.payload))
+	}
+
+	return state
+}
+
+export const removeTabReducer = (state = INITIAL_TERMINAL, action) => {
+	if(action.type === types.REMOVE_TAB){
+
+		return state.delete(action.payload.id)
 	}
 
 	return state
