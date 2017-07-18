3f5a5fd5bae196c738cde70fcaaac77f28bde59e
DavidMBP <steadypathapp@gmail.com>
2017-07-17 21:59:58 -0500

    Added handleCommandEnter to catch errors

--- frontend/components/smart/TerminalContainer.jsx
+++ frontend/components/smart/TerminalContainer.jsx
@@ -5,26 +5,34 @@ import * as terminalActions from 'actions/terminal'
 
 @connect( store => {
 	return {
-		terminalLog: store.terminalLog
+		terminalTabs: store.terminalTabs
 	}
 }, terminalActions)
 export default class TerminalContainer extends React.Component{
 	constructor(props){
 		super(props)
+		this.handleCommandEnter = this.handleCommandEnter.bind(this)
+	}
 
+	handleCommandEnter(command, id){
+		this.props.commandEntered(command, id)
+		.catch(err => {})
 	}
 
 	render(){
 		const {
-			terminalLog,
-			commandEntered
+			terminalTabs,
+			addTab,
+			removeTab
 		} = this.props
 
 
 		return (
 			<Terminal
-				onRequestCommand={commandEntered}
-				terminalLog={terminalLog.toJS()}/>
+				onRequestCommand={this.handleCommandEnter}
+				onRequestAdd={addTab}
+				onRequestRemove={removeTab}
+				terminalTabs={terminalTabs.toJS()}/>
 		)
 	}
 }
