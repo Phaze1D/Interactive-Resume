78f88dc854b18c34f91f55a1ba9f6b2f553b6b8b
DavidMBP <steadypathapp@gmail.com>
2017-07-19 00:11:26 -0500

    Fixed print issue

--- frontend/components/dumb/Terminal/items/Print.jsx
+++ frontend/components/dumb/Terminal/items/Print.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import TerminalItem from './Item'
 
 
-export const Print = ({data, path}) => {
-	var pdfFrame = window.frames['pdf']
-	pdfFrame.focus()
-	pdfFrame.print()
+export const Print = ({data, path, shouldPrint}) => {
+	if(shouldPrint){
+		var pdfFrame = window.frames['pdf']
+		pdfFrame.focus()
+		pdfFrame.print()
+	}
+
 	return (
 		<TerminalItem command={data.command} path={path}>
 		</TerminalItem>
