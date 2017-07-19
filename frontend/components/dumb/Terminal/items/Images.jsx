b05dcc3066c90c19500ac28b1b02da3cc0750439
DavidMBP <steadypathapp@gmail.com>
2017-07-19 00:10:43 -0500

    Created the Images Component

---
+++ frontend/components/dumb/Terminal/items/Images.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TerminalItem from './Item'
+
+
+export const Images = ({data, path}) => {
+
+	const imgList = data.value.map( (img, index) =>
+		<div key={index} className='row-block' style={{textAlign: 'center'}}>
+			<img src={img} className='large-img'/>
+		</div>
+	)
+
+	return (
+		<TerminalItem command={data.command} path={path}>
+			<section className='result-area'>
+				<h4>Images: </h4>
+				{imgList}
+			</section>
+		</TerminalItem>
+	)
+}
