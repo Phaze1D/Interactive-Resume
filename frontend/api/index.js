9770467dc4c4725a8b34318c758b7c986adb9972
DavidMBP <steadypathapp@gmail.com>
2017-07-20 21:46:27 -0500

    Added tabCompletion function

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,5 +1,9 @@
 import local from  './local'
 
-export default function getCommandInfo(command) {
+export function getCommandInfo(command) {
 	return local.get(command)
+}
+
+export function tabCompletion(value) {
+	return local.commands(value)
 }
