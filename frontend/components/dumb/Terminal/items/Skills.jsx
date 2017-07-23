85831834670d0b7a2dae5331d9b19997ca8f4183
DavidMBP <steadypathapp@gmail.com>
2017-07-23 15:17:53 -0500

    Added window resize event for skill bars

--- frontend/components/dumb/Terminal/items/Skills.jsx
+++ frontend/components/dumb/Terminal/items/Skills.jsx
@@ -5,7 +5,26 @@ import TerminalItem from './Item'
 export class Skills extends React.PureComponent{
 	constructor(props){
 		super(props)
+		this.state = {size: window.innerWidth < 445 ? 4 : 2}
+		this.handleResize = this.handleResize.bind(this)
+	}
 
+	componentDidMount() {
+		window.addEventListener('resize', this.handleResize)
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.handleResize)
+	}
+
+	handleResize(event){
+		if(this.state.size === 2 && event.target.innerWidth < 445 ){
+			this.setState({size: 4})
+		}
+
+		if(this.state.size === 4 && event.target.innerWidth > 445){
+			this.setState({size: 2})
+		}
 	}
 
 	render(){
@@ -17,7 +36,7 @@ export class Skills extends React.PureComponent{
 		value.technologies.sort( (a,b) => {return b.mastery - a.mastery})
 
 		const techList = value.technologies.map( (skill, index) =>
-			<SkillBar key={index} skill={skill}/>
+			<SkillBar key={index} skill={skill} size={this.state.size}/>
 		)
 
 		return (
@@ -59,14 +78,14 @@ class SkillBar extends React.Component{
 	}
 
 	render(){
-		const name = this.props.skill.name
+		const {skill,size} = this.props
 		const currentM = this.state.currentM
 
 		return (
 			<article className='flex-row'>
-				<div className='cell sm'>{name}:</div>
+				<div className='cell sm'>{skill.name}:</div>
 				<div className='cell fill'>
-					| { '█'.repeat(Math.round(currentM/2)) }{'░'.repeat(50 - Math.round(currentM/2)) } | {currentM}%
+					| { '█'.repeat(Math.round(currentM/size)) }{'░'.repeat((100/size) - Math.round(currentM/size)) } | {currentM}%
 				</div>
 			</article>
 		)
