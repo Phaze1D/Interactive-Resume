34ac0b40ca821d7dec26abac67534960ab9d6404
DavidMBP <steadypathapp@gmail.com>
2017-07-25 17:37:58 -0500

    Fixed work grammar

--- frontend/api/local/work.js
+++ frontend/api/local/work.js
@@ -10,9 +10,9 @@ const work = {
 			Vida Flor is a health and beauty company that first hired me to help them build an e-commerce website. After finishing the website, they ask me if I could stay full time. I managed their database and created and managed their Point of Sales system to help them organize all their sales and inventory.
 			`,
 			achievements: [
-				'Optimized their website with ReactJS and ExpressJS which changed their previous loading speed from 2s to 0.5s.',
+				'Optimized their website with ReactJS and ExpressJS which changed their previous loading speed from 2s to 0.5s and increased online sales by 25%',
 				'Automated the creation of reports with Microsoft Access which lead to a workforce that was more focus on improving the company instead of having to create reports manually',
-				'Created a POS system which help track inventory and reduce inventory errors by 30%.'
+				'Created a POS system which helps track inventory and reduce inventory errors by 30%.'
 			]
 		}
 	]
