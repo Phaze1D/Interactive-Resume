f3641310c11812a67deaba3a9031c2e79cf81420
DavidMBP <steadypathapp@gmail.com>
2017-07-25 15:46:21 -0500

    Fixed bio grammar

--- frontend/api/local/bio.js
+++ frontend/api/local/bio.js
@@ -1,3 +1,4 @@
+// TODO: Add a new section called personality talk about leading skills and ethics/values
 const bio = {
 	name: 'David Villarreal',
 	role: 'Software Engineer',
@@ -12,7 +13,7 @@ const bio = {
 	},
 
 	about: `
-	Since I was a kid I have always loved to do 2 things, create and solve. I go into computers the moment my dad got one for his work and I immediately wanted to know how it worked in the hopes of building one myself. While research computers I came across the software side of things, which was a perfect fit for me, a way I could create and solve.
+	Since I was a kid I have always loved to do 2 things, create and solve. I got into computers the moment my dad bought one for his work and I immediately wanted to know how they worked in the hopes of building one myself. While researching computers I came across the software side of things, which was a perfect fit for me, a way I could create and solve.
 	`,
 
 	images: ['https://avatars0.githubusercontent.com/u/8495952?v=3'],
